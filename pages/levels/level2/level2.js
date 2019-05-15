2b22f0d41e16938976ea953ab4de6e12e9be15d6
FighterXutao <1603837506qq.com>
2019-05-15 17:00:13 +0800

    M

---
+++ pages/levels/level2/level2.js
@@ -0,0 +1,112 @@
+// pages/levels/level2/level2.js
+Page({
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+
+  data: {
+    routers: [
+      {
+        name: 'HTML',
+        url: '/pages/Course/course',
+        icon: '/image/java_ico.png',
+        code: '10'
+      },
+      {
+        name: 'Java',
+        url: '/pages/Course/course',
+        icon: '/image/python_ico.png',
+        code: '11'
+      },
+      {
+        name: 'CSS',
+        url: '/pages/Course/course',
+        icon: '/image/java_ico.png',
+        code: '10'
+      },
+      {
+        name: 'PHP',
+        icon: '/image/python_ico.png',
+        code: '11'
+      },
+      {
+        name: 'Python',
+        url: '/pages/Course/course',
+        icon: '/image/java_ico.png',
+        code: '10'
+      },
+      {
+        name: 'JavaScript',
+        icon: '/image/python_ico.png',
+        code: '11'
+      },
+      {
+        name: 'C++',
+        url: '/pages/Course/course',
+        icon: '/image/java_ico.png',
+        code: '10'
+      },
+      {
+        name: 'Object-C',
+        icon: '/image/python_ico.png',
+        code: '11'
+      },
+      {
+        name: 'Ruby',
+        url: '/pages/Course/course',
+        icon: '/image/java_ico.png',
+        code: '10'
+      }
+    ]
+  },
+  onLoad: function () {
+    // console.log('onLoad')
+    // var that = this
+  }
+})  
---
+++ pages/stage/stage.js
@@ -0,0 +1,132 @@
+// pages/stage/stage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  toLevel1: function () {
+    wx: wx.redirectTo({
+      url: '../levels/level1/level1',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  toLevel2: function () {
+    wx: wx.redirectTo({
+      url: '../levels/level2/level2',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  toLevel3: function () {
+    wx: wx.redirectTo({
+      url: '../levels/level3/level3',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  toLevel4: function () {
+    wx: wx.redirectTo({
+      url: '../levels/level4/level4',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  toLevel5: function () {
+    wx: wx.redirectTo({
+      url: '../levels/level5/level5',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  toLevel6: function () {
+    wx: wx.redirectTo({
+      url: '../levels/level6/level6',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  toLevel7: function () {
+    wx: wx.redirectTo({
+      url: '../levels/level7/level7',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  toLevel8: function () {
+    wx: wx.redirectTo({
+      url: '../levels/level8/level8',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+
+})
